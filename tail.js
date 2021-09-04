1a0d9ac8476e6623dd1f71ed51d270fd7fb39238
Nzenwa900 <cyrilnze@gmail.com>
2021-09-04 11:43:39 +0100

    conditional statement

--- tail.js
+++ tail.js
@@ -7,3 +7,54 @@
 
     console.log(fullName);
 
+    console.log(5%2);
+
+
+    console.log(5 ** 2)
+
+    var cyrilName = 1;
+
+    console.log(cyrilName);
+
+
+    cyrilName = ++cyrilName;
+    console.log(cyrilName);
+
+    var danName = 5;
+
+    danName = ++danName;
+    console.log(danName);
+
+
+    // if condition
+
+    var dozie = 67;
+
+    if (dozie == '5') {
+        console.log('Yipee!')
+    }
+    
+    // else condition
+
+    if (dozie == 6) {
+        console.log('Dozie is 6')
+    } else {
+        console.log('Dozie is not 6')
+    }
+
+    // else if
+
+
+    if (dozie == 67) {
+        console.log('Dozie is 67')
+    } else if (dozie == 5) {
+        console.log('Dozie is 5')
+    } else {
+        console.log('Dozie is a young lad')
+    }
+    
+// switch condition
+
+
+
+    
