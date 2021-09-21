268dd193d66a45fa440d6a181e19881553852cbc
Nzenwa900 <cyrilnze@gmail.com>
2021-09-21 13:25:34 +0100

    javascript object practice

---
+++ assignment.js
@@ -0,0 +1,18 @@
+
+
+/* CREATE A CAR OBJECT properties should include name of car, model, year of manufacture, 
+start function, stop function.*/
+
+const car = {
+    carName:"corolla",
+    carModel:"Toyota",
+    yom:"1990",
+    startFunction:"key start",
+    breakFunction:"automatic",
+    carColor:"black",
+    Transmission:"two minutes",
+    dashboard: "luxurious",
+
+};
+
+console.log(car.startFunction);
